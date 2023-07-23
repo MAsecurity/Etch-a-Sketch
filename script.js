32c5abb9467736a21770543aead24ded082bfcdb
MAsecurity <108958617+MAsecurity@users.noreply.github.com>
2023-07-23 09:50:46 +0100

    Randomize drawing if random button is clicked

--- script.js
+++ script.js
@@ -1,11 +1,4 @@
-// Will require 16 by 16 grid of square divs as default
-// Can then also prompt user to change the grid of square divs
-// Ideally will allow user to use mouse and draw on the divs
-// By default color will be black  
-// Will include hovering which changes colour of div
-// Will give user the option to select random which will Randomize the colour 
-// of the square divs when drawing 
-
+let color = 'black';
 let selectBtn = document.querySelector(".select");
 let container = document.querySelector('.containerForBoard');
 // Default create 16 by 16 board
@@ -17,7 +10,7 @@ selectBtn.addEventListener("click", () => {
     alert("Please enter between 0 and 100");
     userChoice = +prompt("Enter the size of the board\nNote maximum is 100");
   }else {
-    getUserChoice(userChoice);
+    createBoard(userChoice);
 
   }
   
@@ -31,51 +24,70 @@ function createBoard(size){
   for (let i=0; i<numberOfDivs; i++) {
     let newDiv = document.createElement('div');
     container.appendChild(newDiv);
-    mouseOver(newDiv);
-    
+    newDiv.addEventListener("mouseover", colorDiv);
+   
   }
 }
 
-function getUserChoice (size) {
-  container.style.cssText = `grid-template-columns: repeat(${size},1fr); grid-template-rows:repeat(${size},1fr);`;
-  //Create the divs that will fill the board
-  let numberOfDivs = size * size;
-  for (let i=0; i<numberOfDivs; i++) {
-    let newDiv = document.createElement('div');
-    container.appendChild(newDiv);
-    mouseOver(newDiv);
-    
+function colorDiv() {
+  if (color === 'random') {
+    // randomize the color of the divs when the random button is clicked
+    let randomNumber1 = Math.floor(Math.random() * 256); // returns values 0 to 255
+    let randomNumber2 = Math.floor(Math.random() * 256);
+    let randomNumber3 = Math.floor(Math.random() * 256);
+    this.style.backgroundColor = `rgb(${randomNumber1},${randomNumber2},${randomNumber3})`;
+  }else {
+    this.style.cssText = "background-color: black";
   }
+
+};
+
+function setColor(colorChoice) {
+  color = colorChoice;
+
 }
 
-function mouseOver(newDiv) {
-  let blackBtn = document.querySelector(".black");
-  let resetBtn = document.querySelector(".reset");
-  //Added a hover class
-  newDiv.classList.add("changeColour");
-  // if black button is clicked moving over a button will change colour to black permenatly
-  blackBtn.addEventListener("click", () => {
-    newDiv.addEventListener("mouseover", () => {
-      newDiv.style.cssText = "background-color: black";
-    });
+//Reset all values inside the cotainer
+function resetBoard() {
+  let allDivs = container.querySelectorAll('div');
+  allDivs.forEach(div => {
+    div.style.cssText="#e4e0e0";
+  });
+}
+
+
+// function mouseOver(newDiv) {
+//   let blackBtn = document.querySelector(".black");
+//   let resetBtn = document.querySelector(".reset");
+//   let randomBtn = document.querySelector(".random")
   
+//   // Added a hover class that only changes div colour when it's hovered and stops when it's not.
+//   newDiv.classList.add("changeColour");
 
-  });
-// reset the values when the reset button is clicked
-  resetBtn.addEventListener("click", () => {
-    newDiv.style.cssText = "background-colour: #e4e0e0";
-  });
+//   // if black button is clicked, moving over the div will change the color to black
+//   blackBtn.addEventListener("click", () => {
+//     newDiv.addEventListener("mouseover", () =>{
+//       newDiv.style.backgroundColor = "black";
 
-// randomize the colour of the divs when the random button is clicked
-  Math.floor(Math.random() * 256);
-  // Will generate
-
-
-}
-
-
-
+//     });
+//   });
+//   // reset the values when the reset button is clicked
+//   resetBtn.addEventListener("click", () => {
+//     newDiv.style.backgroundColor = "#e4e0e0";
+//   });
 
 
+//   // Event listener for the "Randomize Color" button
+//   randomBtn.addEventListener("click", () => {
+//     newDiv.addEventListener("mouseover", ()=>{
+//        // randomize the color of the divs when the random button is clicked
+//       let randomNumber1 = Math.floor(Math.random() * 256);
+//       let randomNumber2 = Math.floor(Math.random() * 256);
+//       let randomNumber3 = Math.floor(Math.random() * 256);
+//       div.style.backgroundColor = `rgb(${randomNumber1},${randomNumber2},${randomNumber3})`;
+//     });
 
+//   });
+
+// }
 

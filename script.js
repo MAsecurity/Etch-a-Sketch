8c4ff3212931befc233217dcc9b93fc9028684a4
MAsecurity <108958617+MAsecurity@users.noreply.github.com>
2023-07-23 06:59:42 +0100

    Change colour of the divs to black when hovered
    - changed the colour of the the divs when the black button is clicked,
      and the div's inside the board are hovered against.

--- script.js
+++ script.js
@@ -8,13 +8,14 @@
 
 let selectBtn = document.querySelector(".select");
 let container = document.querySelector('.containerForBoard');
+// Default create 16 by 16 board
   createBoard(16);
-
+// Extract user choice if the select button is clicekd
 selectBtn.addEventListener("click", () => {
-  let userChoice = +prompt("Enter the size of the board\n Note maximum is 100");
+  let userChoice = +prompt("Enter the size of the board\nNote maximum is 100");
   if (userChoice < 0  || userChoice > 100) {
     alert("Please enter between 0 and 100");
-    userChoice = +prompt("Enter the size of the board\n Note maximum is 100");
+    userChoice = +prompt("Enter the size of the board\nNote maximum is 100");
   }else {
     getUserChoice(userChoice);
 
@@ -23,13 +24,40 @@ selectBtn.addEventListener("click", () => {
 
 });
 
+function createBoard(size){
+  container.style.cssText = `grid-template-columns: repeat(${size},1fr); grid-template-rows:repeat(${size},1fr);`;
+  //Create the divs that will fill the board
+  let numberOfDivs = size * size;
+  for (let i=0; i<numberOfDivs; i++) {
+    let newDiv = document.createElement('div');
+    container.appendChild(newDiv);
+    mouseOver(newDiv);
+    
+  }
+}
 
 function getUserChoice (size) {
   container.style.cssText = `grid-template-columns: repeat(${size},1fr); grid-template-rows:repeat(${size},1fr);`;
-
+  //Create the divs that will fill the board
+  let numberOfDivs = size * size;
+  for (let i=0; i<numberOfDivs; i++) {
+    let newDiv = document.createElement('div');
+    container.appendChild(newDiv);
+    mouseOver(newDiv);
+    
+  }
 }
-function createBoard(size){
-  container.style.cssText = `grid-template-columns: repeat(${size},1fr); grid-template-rows:repeat(${size},1fr);`;
+
+function mouseOver(newDiv) {
+  let blackBtn = document.querySelector(".black");
+  blackBtn.addEventListener("click", () => {
+    newDiv.addEventListener("mouseover", () => {
+      newDiv.style.cssText = "background-color: black";
+    });
+  
+
+  });
+
 
 }
 

2b8901398e8d87690cbc74e7030baf3529f527cc
MAsecurity <108958617+MAsecurity@users.noreply.github.com>
2023-07-23 02:18:35 +0100

    Write some planning and psuedocode in the js file

---
+++ script.js
@@ -0,0 +1,7 @@
+// Will require 16 by 16 grid of square divs as default
+// Can then also prompt user to change the grid of square divs
+// Ideally will allow user to use mouse and draw on the divs
+// By default color will be black  
+// Will include hovering which changes colour of div
+// Will give user the option to select random which will Randomize the colour 
+// of the square divs when drawing 
